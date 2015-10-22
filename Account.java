796324dc4ebae2129f855412712c7ba5cba530e5
Marc <marc.chesebro@gmail.com>
2015-10-22 00:30:09 -0400

    Added simple constructors

--- Account.java
+++ Account.java
@@ -5,14 +5,27 @@ import java.util.GregorianCalendar;
 
 public abstract class Account implements Serializable {
 
-	   private static final long serialVersionUID = 1L;
-	   private int number;
-	   private String owner;
-	   private GregorianCalendar dateOpened;
-	   private double balance;
+	private static final long serialVersionUID = 1L;
+	//we could make these protected if we wanted easier access
+	private int number;
+	private String owner;
+	private GregorianCalendar dateOpened;
+	private double balance;
 
-	   // add constructor
+	// add constructor
 
-	   // add getter, setter, equals(), and toString() methods
+	//default
+	public Account() {}
+
+	//all Parameters
+	public Account(int number, String owner, GregorianCalendar dateOpened, double balance) {
+
+		this.number = number;
+		this.owner = owner;
+		this.dateOpened = dateOpened;
+		this.balance = balance;
+	}
+
+	// add getter, setter, equals(), and toString() methods
 
 	}
