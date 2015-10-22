d635e1c593c0962147fd67aedcfa9f5c2afa50dd
Marc <marc.chesebro@gmail.com>
2015-10-22 00:44:20 -0400

    added simple constructor for all parameters

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,13 +1,20 @@
 package BankProgram;
 
+import java.util.GregorianCalendar;
+
 public class CheckingAccount extends Account {
 
-	   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	   
-	   private double monthlyFee;
+	private double monthlyFee;
 
-	   // add constructor
+	// add constructor
 
-	   // add getter, setter, equals(), and toString() methods
+	public CheckingAccount(int number, String owner, GregorianCalendar dateOpened, double balance, double monthlyFee) {
+		super(number, owner, dateOpened, balance);
+		this.monthlyFee = monthlyFee;
+	}
+
+	// add getter, setter, equals(), and toString() methods
 
 	}
