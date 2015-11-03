e8f5b92276b65f83116bf624a46b78880e0c22a9
David Lamar <dmlamar@mtu.edu>
2015-11-02 20:28:06 -0500

    More style guide for BankModel

--- BankModel.java
+++ BankModel.java
@@ -127,9 +127,12 @@ public class BankModel extends AbstractTableModel {
 		case 2:
 			return temp.getOwner();
 		case 3:
-			return (temp.getDateOpened().get(GregorianCalendar.MONTH)+1) + "/" +
-				temp.getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) + "/"
-				+ temp.getDateOpened().get(GregorianCalendar.YEAR);
+			return (
+				temp.getDateOpened().get(GregorianCalendar.MONTH)+1) + 
+				"/" +
+				temp.getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) 
+				+ "/" +
+				temp.getDateOpened().get(GregorianCalendar.YEAR);
 		case 4:
 			return temp.getBalance();
 		case 5:
@@ -158,9 +161,11 @@ public class BankModel extends AbstractTableModel {
 //	@Override
 //	public Object getElementAt(int arg0) {
 //
-//		return String.format("Account Number: %s Name: %s Balance: %s Date: %s", 
+//		return String.format("Account Number: %s Name: %s Balance: 
+//              %s Date: %s", 
 //				acts.get(arg0).getNumber(), acts.get(arg0).getOwner(), 
-//				acts.get(arg0).getBalance(), acts.get(arg0).getDateOpened().getTime());
+//				acts.get(arg0).getBalance(), 
+//              acts.get(arg0).getDateOpened().getTime());
 //	}
 //
 //	@Override
@@ -228,7 +233,8 @@ public class BankModel extends AbstractTableModel {
 					acts.get(i).getNumber(),
 					acts.get(i).getOwner(),
 					//format date
-					new SimpleDateFormat("MM/dd/YY").format(acts.get(i).getDateOpened().getTime()) ,
+					new SimpleDateFormat("MM/dd/YY")
+			.format(acts.get(i).getDateOpened().getTime()) ,
 					acts.get(i).getBalance(),
 					((CheckingAccount) acts.get(i)).getMonthlyFee());
 		}else{
@@ -236,7 +242,8 @@ public class BankModel extends AbstractTableModel {
 					acts.get(i).getNumber(),
 					acts.get(i).getOwner(),
 					//format date
-					new SimpleDateFormat("MM/dd/YY").format(acts.get(i).getDateOpened().getTime()) ,
+					new SimpleDateFormat("MM/dd/YY")
+			.format(acts.get(i).getDateOpened().getTime()) ,
 					acts.get(i).getBalance(),
 					((SavingsAccount) acts.get(i)).getMinBalance(),
 					((SavingsAccount) acts.get(i)).getInterestRate());
@@ -307,7 +314,10 @@ public class BankModel extends AbstractTableModel {
 					temp = new CheckingAccount(
 							Integer.parseInt(info[1]), 
 							info[2], 
-							new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[0]), Integer.parseInt(date[1])), 
+							new GregorianCalendar(
+									Integer.parseInt(date[2]), 
+									Integer.parseInt(date[0]), 
+									Integer.parseInt(date[1])), 
 							Double.parseDouble(info[4]),
 							Double.parseDouble(info[5]));
 					
@@ -317,7 +327,10 @@ public class BankModel extends AbstractTableModel {
 					temp = new SavingsAccount(
 							Integer.parseInt(info[1]),
 							info[2],
-							new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[0]), Integer.parseInt(date[1])),
+							new GregorianCalendar(
+									Integer.parseInt(date[2]), 
+									Integer.parseInt(date[0]), 
+									Integer.parseInt(date[1])),
 							Double.parseDouble(info[4]),
 							Double.parseDouble(info[5]),
 							Double.parseDouble(info[6]));
@@ -494,34 +507,45 @@ public class BankModel extends AbstractTableModel {
 		for(int i = 0; i < acts.size(); i++){
 			
 			//checks if it's savings or checkings
-			if(acts.get(i).getClass().getName().equals("BankProgram.CheckingAccount")){
+			if(acts.get(i).getClass().getName()
+					.equals("BankProgram.CheckingAccount")){
 				write += acts.get(i).getClass().getName() + "|";
 				write += acts.get(i).getNumber() + "|";
 				write += acts.get(i).getOwner() + "|";
 				write += 
-						acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) + "/" +
-						acts.get(i).getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) +
-						"/"+acts.get(i).getDateOpened().get(GregorianCalendar.YEAR) + "|";
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.MONTH) + "/" +
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.DAY_OF_MONTH) +
+						"/"+acts.get(i).getDateOpened()
+						.get(GregorianCalendar.YEAR) + "|";
 				write += acts.get(i).getBalance() + "|";
-				write += ((CheckingAccount)acts.get(i)).getMonthlyFee() + "\n";
+				write += ((CheckingAccount)acts.get(i)).getMonthlyFee() 
+						+ "\n";
 			}else{
 				write += acts.get(i).getClass().getName() + "|";
 				write += acts.get(i).getNumber() + "|";
 				write += acts.get(i).getOwner() + "|";
 				write += 
-						acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) + "/" +
-						acts.get(i).getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) +
-						"/"+acts.get(i).getDateOpened().get(GregorianCalendar.YEAR) + "|";
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.MONTH) + "/" +
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.DAY_OF_MONTH) +
+						"/"+acts.get(i).getDateOpened()
+						.get(GregorianCalendar.YEAR) + "|";
 				write += acts.get(i).getBalance() + "|";
-				write += ((SavingsAccount)acts.get(i)).getMinBalance() + "|";
-				write += ((SavingsAccount)acts.get(i)).getInterestRate() + "\n";
+				write += ((SavingsAccount)acts.get(i)).getMinBalance() 
+						+ "|";
+				write += ((SavingsAccount)acts.get(i)).getInterestRate() 
+						+ "\n";
 			}
 			
 		}
 		
 		
 		try {
-			BufferedWriter bf = new BufferedWriter(new FileWriter(fileName));
+			BufferedWriter bf = 
+					new BufferedWriter(new FileWriter(fileName));
 			bf.write(write);
 			
 			bf.close();
@@ -544,28 +568,51 @@ public class BankModel extends AbstractTableModel {
 		for(int i = 0; i < acts.size(); i++){
 			
 			//checks if it's savings or checking
-			if(acts.get(i).getClass().getName().equals("BankProgram.CheckingAccount")){
+			if(acts.get(i).getClass().getName().equals(
+					"BankProgram.CheckingAccount")){
 				write += "<account>\n";
-				write += "<type>\n" + acts.get(i).getClass().getSimpleName() + "\n</type>\n";
-				write += "<number>\n" + acts.get(i).getNumber() + "\n</number>\n";
-				write += "<owner>\n" + acts.get(i).getOwner() + "\n</owner>\n";
-				write += "<date>\n" + acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) + "/" +
-						acts.get(i).getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) +
-						"/"+acts.get(i).getDateOpened().get(GregorianCalendar.YEAR) + "\n</date>\n";
-				write += "<balance>\n" + acts.get(i).getBalance() + "\n</balance>\n";
-				write += "<fee>\n" + ((CheckingAccount)acts.get(i)).getMonthlyFee() + "\n</fee>\n";
+				write += "<type>\n" + 
+				acts.get(i).getClass().getSimpleName() + "\n</type>\n";
+				write += "<number>\n" + 
+				acts.get(i).getNumber() + "\n</number>\n";
+				write += "<owner>\n" + 
+				acts.get(i).getOwner() + "\n</owner>\n";
+				write += "<date>\n" + 
+				acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) 
+				+ "/" +
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.DAY_OF_MONTH) +
+						"/"+acts.get(i).getDateOpened()
+						.get(GregorianCalendar.YEAR) + "\n</date>\n";
+				write += "<balance>\n" + acts.get(i).getBalance() + 
+						"\n</balance>\n";
+				write += "<fee>\n" + ((CheckingAccount)acts.get(i))
+						.getMonthlyFee() + "\n</fee>\n";
 				write += "</account>\n";
 			}else{
 				write += "<account>\n";
-				write += "<type>\n" +acts.get(i).getClass().getSimpleName() + "\n</type>\n";
-				write += "<number>\n" +acts.get(i).getNumber() + "\n</number>\n";
-				write += "<owner>\n" +acts.get(i).getOwner() + "\n</owner>\n";
-				write += "<date>\n" + acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) + "/" +
-						acts.get(i).getDateOpened().get(GregorianCalendar.DAY_OF_MONTH) +
-						"/"+acts.get(i).getDateOpened().get(GregorianCalendar.YEAR) + "\n</date>\n";
-				write += "<balance>\n" +acts.get(i).getBalance() + "\n</balance>\n";
-				write += "<min>\n" +((SavingsAccount)acts.get(i)).getMinBalance() + "\n</min>\n";
-				write += "<interest>\n" +((SavingsAccount)acts.get(i)).getInterestRate() + "\n</interest>\n";
+				write += "<type>\n" +
+				acts.get(i).getClass().getSimpleName() + "\n</type>\n";
+				write += "<number>\n" +
+				acts.get(i).getNumber() + "\n</number>\n";
+				write += "<owner>\n" +
+				acts.get(i).getOwner() + "\n</owner>\n";
+				write += "<date>\n" + 
+				acts.get(i).getDateOpened().get(GregorianCalendar.MONTH) 
+				+ "/" +
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.DAY_OF_MONTH) +
+						"/"+
+						acts.get(i).getDateOpened()
+						.get(GregorianCalendar.YEAR) + "\n</date>\n";
+				write += "<balance>\n" +
+						acts.get(i).getBalance() + "\n</balance>\n";
+				write += "<min>\n" +
+						((SavingsAccount)acts.get(i)).getMinBalance() + 
+						"\n</min>\n";
+				write += "<interest>\n" +
+						((SavingsAccount)acts.get(i)).getInterestRate() 
+						+ "\n</interest>\n";
 				write += "</account>\n";
 			}
 			
@@ -573,7 +620,8 @@ public class BankModel extends AbstractTableModel {
 		
 		
 		try {
-			BufferedWriter bf = new BufferedWriter(new FileWriter(fileName));
+			BufferedWriter bf = 
+					new BufferedWriter(new FileWriter(fileName));
 			bf.write(write);
 			
 			bf.close();
